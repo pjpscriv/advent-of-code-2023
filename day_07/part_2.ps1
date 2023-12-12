a02da727f7b8885efd1f236f42a9facd6fb8abf4
Peter Scriven <peter.scriven@trademe.co.nz>
2023-12-12 22:48:38 +1300

    day 7.2 prelim

--- day_07/part_2.ps1
+++ day_07/part_2.ps1
@@ -1,10 +1,132 @@
-$lines = get-content -Path 'input.txt'
-
-$output = ""
-
-foreach ($line in $lines) {
-    $output = $output + $line
-    write-host $line
+enum HandScores {
+    FiveOfKind  = 1
+    FourOfKind  = 2
+    FullHouse   = 3
+    ThreeOfKind = 4
+    TwoPair     = 5
+    OnePair     = 6
+    HighCard    = 7
 }
 
-write-host $output
+$CARD_LIST = @('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+
+function countChars($string) {
+    $uniques = $string.toCharArray() | select-object -unique
+    $dict = @{}
+    foreach ($char in $uniques) {
+        $count = ($string.ToCharArray() | Where-Object {$_ -eq $char}).count
+        $dict[$char] = $count
+    }
+    return $dict
+}
+
+function getJokerPositions($string) {
+    return [regex]::Matches($string, 'J').Index
+}
+
+function getHandScore($hand) {
+    $countDict = countChars($hand)
+
+    $counts = $countDict.Values
+    $maxCount = $counts | Sort-Object | Select-Object -Last 1
+    switch ($maxCount) {
+        5 { return [HandScores]::FiveOfKind }
+        4 { return [HandScores]::FourOfKind }
+        3 {
+            if ($counts -contains 2) {
+                return [HandScores]::FullHouse
+            }
+            return [HandScores]::ThreeOfKind
+        }
+        2 {
+            if (($counts | Where-Object {$_ -eq 2}).count -eq 2) {
+                return [HandScores]::TwoPair
+            }
+            return [HandScores]::OnePair
+        }
+        default {
+            return [HandScores]::HighCard
+        }
+    }    
+}
+
+function getMaxHandScore($hand) {
+    $jokers = getJokerPositions($hand)
+    
+    if ($jokers.Length -eq 0) {
+        return getHandScore($hand)
+    }
+
+
+    $jokerOptions = @()
+    foreach ($index in $jokers) {
+        $jokerOptions += @{
+            index = $index
+            options = $CARD_LIST
+        }
+    }
+
+    $maxScore = [HandScores]::HighCard
+    $testHand = $hand
+    foreach ($joker in $jokerOptions) {
+
+
+    }8
+
+}
+
+function transformToSortable($cards) {
+    $cards = $cards.Replace("A", "A")
+    $cards = $cards.Replace("K", "B")
+    $cards = $cards.Replace("Q", "C")
+    $cards = $cards.Replace("T", "D")
+    $cards = $cards.Replace("9", "E")
+    $cards = $cards.Replace("8", "F")
+    $cards = $cards.Replace("7", "G")
+    $cards = $cards.Replace("6", "H")
+    $cards = $cards.Replace("5", "I")
+    $cards = $cards.Replace("4", "J")
+    $cards = $cards.Replace("3", "K")
+    $cards = $cards.Replace("2", "L")
+    $cards = $cards.Replace("J", "M")
+    return $cards
+}
+
+$lines = get-content -Path 'input.txt'
+
+$hands = @()
+foreach ($line in $lines) {
+    $parts = $line.split(' ')
+    $cards = $parts[0]
+    $bid = [int] $parts[1]
+
+    $score = getMaxHandScore($cards)
+    # Write-Host $score
+    
+    $hand = @{
+        score = $score
+        cards = $cards
+        sortHand = transformToSortable($cards)
+        bid = $bid
+    }
+
+    $hands += $hand
+}
+    
+
+write-host
+$hands = $hands | Sort-Object -Property { $_.score }, { $_.sortHand }
+
+# Do Summing
+$sum = 0
+$multiplier = $lines.length
+foreach ($hand in $hands) {
+    Write-Host $hand.score, $hand.cards, $hand.bid, $multiplier, $hand.sortHand
+    $sum += ($hand.bid * $multiplier)
+    # write-host $sum
+    $multiplier -= 1
+}
+
+Write-Host $sum
+
+
